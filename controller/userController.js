300b2a090fcbd1fb0c56debdc521fc1bb439d8d5
umair1126 <53148857+umair1126@users.noreply.github.com>
2021-09-24 10:01:21 +0500

    update in controller

--- controller/userController.js
+++ controller/userController.js
@@ -72,7 +72,7 @@ exports.updateMe = catchAsync(async (req, res, next) => {
   if (req.body.password || req.body.passwordConfirm) {
     return next(
       new AppError(
-        "This route is not for password updates. Please use /updateMyPassword.",
+        "This route is not for password updates. Please use /updateMyPassword route.",
         400
       )
     );
